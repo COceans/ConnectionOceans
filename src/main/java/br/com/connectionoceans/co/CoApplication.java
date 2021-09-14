4fe43053cb0bde20e4a05e0f0b9c25f88be40317
DanielMendes010 <danielinacio101@gmail.com>
2021-09-14 11:15:30 -0300

    Deploy CO

--- src/main/java/br/com/connectionoceans/co/CoApplication.java
+++ src/main/java/br/com/connectionoceans/co/CoApplication.java
@@ -1,13 +1,23 @@
 package br.com.connectionoceans.co;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class CoApplication {
+@GetMapping
+public ModelAndView swaggerUi() {
+return new ModelAndView("redirect:/swagger-ui/");
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoApplication.class, args);
-	}
+}
 
+public static void main(String[] args) {
+SpringApplication.run(CoApplication.class, args);
+
+}
 }
